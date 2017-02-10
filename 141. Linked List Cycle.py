f2bfac68992e98539d94d92093b433e408ee8750
anqijiyu <1979047142@qq.com>
2017-02-10 22:34:56 +0800

    Update 141. Linked List Cycle.py

--- 141. Linked List Cycle.py
+++ 141. Linked List Cycle.py
@@ -11,7 +11,7 @@ class Solution(object):
         :rtype: bool
         """
         fast=slow=head
-        if fast and fast.next:
+        while fast and fast.next:
             fast=fast.next.next
             slow=slow.next
             if fast==slow:
