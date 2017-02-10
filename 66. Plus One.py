9863fc0d1bbcad3d6a471f6107188076a84cdb9e
anqijiyu <1979047142@qq.com>
2017-02-10 22:00:02 +0800

    silu

--- 66. Plus One.py
+++ 66. Plus One.py
@@ -6,3 +6,24 @@ class Solution(object):
         """
         num = reduce(lambda x, y: x*10+y, digits)
         return [int(i) for i in str(num+1)]
+    
+    class Solution(object):
+    def plusOne(self, digits):
+        """
+        :type digits: List[int]
+        :rtype: List[int]
+        """
+        ans=[]
+        count=0
+        digits[len(digits)-1]+=1
+        for i in digits[::-1]:
+            tmp=i+count
+            if tmp>9:
+                ans.insert(0,tmp-10)
+                count=1
+            else:
+                ans.insert(0,tmp)
+                count=0
+        if count:
+            ans.insert(0,count)
+        return ans
